a68bc560a6dd8f1c671a405269be42d3bf1bd564
김가린 <garin0112@naver.com>
2022-04-16 13:51:05 +0900

    refactor(result): 게임 결과 출력 method 분리

--- src/main/java/baseball/domain/GameResult.java
+++ src/main/java/baseball/domain/GameResult.java
@@ -17,26 +17,41 @@ public class GameResult {
 
     // 게임 결과 프린트
     public void printGameResult() {
+        printAllStrike();
+        printNothing();
+        String message = "";
+        message = printBall(message);
+        message = printStrike(message);
+        System.out.println(message);
+        return;
+    }
+
+    private void printAllStrike() {
         if (strikeCount == BaseBall.BASEBALL_SIZE) {
             System.out.println(strikeCount + Message.STRIKE_MESSAGE);
             return;
         }
+    }
 
+    private void printNothing() {
         if (strikeCount == 0 && ballCount == 0) {
             System.out.println(Message.NOTHING);
             return;
         }
+    }
 
-        String message = "";
+    private String printBall(String message) {
         if (ballCount > 0) {
             message += ballCount + Message.BALL_MESSAGE;
         }
+        return message;
+    }
+
+    private String printStrike(String message) {
         if (strikeCount > 0) {
             message += addWhiteSpace(message) + strikeCount + Message.STRIKE_MESSAGE;
         }
-
-        System.out.println(message);
-        return;
+        return message;
     }
 
     private String addWhiteSpace(String message) {
