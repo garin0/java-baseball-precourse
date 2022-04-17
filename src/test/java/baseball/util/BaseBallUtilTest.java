7dfbc7faff7abdf996f40b0a3b876f0090c50b58
김가린 <garin0112@naver.com>
2022-04-17 22:03:22 +0900

    refactor(): 테스트명 변경

--- src/test/java/baseball/util/BaseBallUtilTest.java
+++ src/test/java/baseball/util/BaseBallUtilTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class BaseBallUtilTest {
     @Test
     @DisplayName("문자열 Array로 변경")
-    void addStringToArray() {
+    void changeStringToArray() {
         String numStr = "326";
         List<Integer> integerList = StringUtil.toArray(numStr);
         assertThat(integerList.size()).isEqualTo(BaseBallAnswerGenerator.BASEBALL_SIZE);
